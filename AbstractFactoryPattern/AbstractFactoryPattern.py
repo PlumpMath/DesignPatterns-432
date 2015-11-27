16e3d87297536338ca253d08ee70ea10624922e9
muupu <once645@gmail.com>
2015-11-27 20:00:08 +0800

    create_diagram function

--- AbstractFactoryPattern/AbstractFactoryPattern.py
+++ AbstractFactoryPattern/AbstractFactoryPattern.py
@@ -1,6 +1,20 @@
 
 
 
+
+
+
+def create_diagram(factory):
+	# create_diagram无需关心参数factory具体是什么
+	# 只要知道factoryj具有所需的接口（make_diagram，make_rectangle，make_text）
+	diagram = factory.make_diagram(30, 7);
+	rectangle = factory.make_rectangle(4, 1, 22, 5, "yellow")
+	text = factory.make_text(7, 3, "Abstract Factory")
+	diagram.add(rectangle)
+	diagram.add(text)
+	return diagram
+
+
 def main():
 	# 省略了创建两个文件(textFilename、svgFilename)的操作
 
