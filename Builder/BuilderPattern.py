1ce8d3714631f6f4f2d33b0550fa31a2d1f99679
muupu <once645@gmail.com>
2015-11-28 21:54:58 +0800

    abstract method

--- Builder/BuilderPattern.py
+++ Builder/BuilderPattern.py
@@ -1,5 +1,5 @@
 
-
+import abc
 import os
 import sys
 
@@ -22,3 +22,29 @@ def create_login_form(builder):
     builder.add_button("Login", 2, 0)
     builder.add_button("Cancel", 2, 1)
     return builder.form()
+
+class AbstractFormBuilder(metaclass=abc.ABCMeta):
+	# 继承自AbstractFormBuilder的类必须实现所有抽象方法
+    @abc.abstractmethod
+    def add_title(self, title):
+        self.title = title
+
+
+    @abc.abstractmethod
+    def form(self):
+        pass
+
+
+    @abc.abstractmethod
+    def add_label(self, text, row, column, **kwargs):
+        pass
+
+
+    @abc.abstractmethod
+    def add_entry(self, variable, row, column, **kwargs):
+        pass
+
+
+    @abc.abstractmethod
+    def add_button(self, text, row, column, **kwargs):
+        pass
