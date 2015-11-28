826139ad842253117dbaa9490733e14ee87ec56d
muupu <once645@gmail.com>
2015-11-28 22:08:59 +0800

    htmlformbuilder

--- Builder/BuilderPattern.py
+++ Builder/BuilderPattern.py
@@ -24,6 +24,7 @@ def create_login_form(builder):
     return builder.form()
 
 class AbstractFormBuilder(metaclass=abc.ABCMeta):
+	# metaclass为abc.ABCMeta，则该类无法初始化，只能作为抽象基类使用
 	# 继承自AbstractFormBuilder的类必须实现所有抽象方法
     @abc.abstractmethod
     def add_title(self, title):
@@ -48,3 +49,27 @@ class AbstractFormBuilder(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def add_button(self, text, row, column, **kwargs):
         pass
+
+class HtmlFormBuilder(AbstractFormBuilder):
+
+    def __init__(self):
+        self.title = "HtmlFormBuilder"
+        self.items = {}
+
+
+    def add_title(self, title):
+        super().add_title(escape(title))
+
+
+    def add_label(self, text, row, column, **kwargs):
+        self.items[(row, column)] = ('<td><label for="{}">{}:</label></td>'
+                .format(kwargs["target"], escape(text)))
+
+
+    def add_entry(self, variable, row, column, **kwargs):
+        html = """<td><input name="{}" type="{}" /></td>""".format(
+                variable, kwargs.get("kind", "text"))
+        self.items[(row, column)] = html
+
+
+    
