9afdb5313ba187c8b0a1fffeb6bcd27bb1042f08
muupu <once645@gmail.com>
2015-11-28 21:50:43 +0800

    create_login_form

--- Builder/BuilderPattern.py
+++ Builder/BuilderPattern.py
@@ -1,5 +1,8 @@
 
 
+import os
+import sys
+
 def main():
 	# 创建两个表单，并分别填入对应的文件中
 	htmlForm = create_login_form(HtmlForBuilder())
@@ -9,3 +12,13 @@ def main():
 	tkForm = create_login_form(TkFormBuilder())
 	with open(tkFilename, "w", encoding="utf-8") as file:
 		file.write(tkForm)
+
+def create_login_form(builder):
+    builder.add_title("Login")
+    builder.add_label("Username", 0, 0, target="username")
+    builder.add_entry("username", 0, 1)
+    builder.add_label("Password", 1, 0, target="password")
+    builder.add_entry("password", 1, 1, kind="password")
+    builder.add_button("Login", 2, 0)
+    builder.add_button("Cancel", 2, 1)
+    return builder.form()
