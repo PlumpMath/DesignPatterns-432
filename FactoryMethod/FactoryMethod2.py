7c6420e5525b84ef31ffbc5deae64b4768660068
muupu <once645@gmail.com>
2015-12-05 22:38:27 +0800

    CheckersBoard

--- FactoryMethod/FactoryMethod2.py
+++ FactoryMethod/FactoryMethod2.py
@@ -1,5 +1,12 @@
 
+import io
+import itertools
+import os
+import sys
+import tempfile
+import unicodedata
 
+BLACK, WHITE = ("BLACK", "WHITE")
 
 def main():
     checkers = CheckersBoard()
@@ -13,3 +20,37 @@ def main():
         with open(filename, "w", encoding="utf-8") as file:
             file.write(sys.stdout.getvalue())
         print("wrote '{}'".format(filename), file=sys.__stdout__)
+
+class AbstractBoard:
+
+    def __init__(self, rows, columns):
+        self.board = [[None for _ in range(columns)] for _ in range(rows)]
+        self.populate_board()
+
+
+    def populate_board(self):
+        raise NotImplementedError()
+
+
+    def __str__(self):
+        squares = []
+        for y, row in enumerate(self.board):
+            for x, piece in enumerate(row):
+                square = console(piece, BLACK if (y + x) % 2 else WHITE)
+                squares.append(square)
+            squares.append("\n")
+        return "".join(squares)
+
+class CheckersBoard(AbstractBoard):
+
+    def __init__(self):
+        super().__init__(10, 10)
+
+
+    def populate_board(self):
+        for x in range(0, 9, 2):
+            for y in range(4):
+                column = x + ((y + 1) % 2)
+                for row, color in ((y, "black"), (y + 6, "white")):
+                    self.board[row][column] = create_piece("draught",
+                            color)
