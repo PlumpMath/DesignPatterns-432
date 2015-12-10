a6e2fbe05d02603d26fb9b90486abe50637a040a
muupu <once645@gmail.com>
2015-12-10 23:44:57 +0800

    make new method

--- FactoryMethod/FactoryMethod3.py
+++ FactoryMethod/FactoryMethod3.py
@@ -18,3 +18,13 @@ def main():
         with open(filename, "w", encoding="utf-8") as file:
             file.write(sys.stdout.getvalue())
         print("wrote '{}'".format(filename), file=sys.__stdout__)
+
+class Piece(str):
+
+    __slots__ = ()
+
+
+def make_new_method(char): # Needed to create a fresh method each time
+    def new(Class): # Can't use super() or super(Piece, Class)
+        return Piece.__new__(Class, char)
+    return new
