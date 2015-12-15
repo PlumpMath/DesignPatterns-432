6c9cc8872a7244a1f2f20fe821b5287c7d5a0260
muupu <once645@gmail.com>
2015-12-15 23:13:24 +0800

    populate_board

--- FactoryMethod/FactoryMethod3.py
+++ FactoryMethod/FactoryMethod3.py
@@ -62,3 +62,16 @@ class AbstractBoard:
 
     def create_piece(self, kind, color):
         return AbstractBoard.__classForPiece[kind, color]()
+
+    def populate_board(self):
+        raise NotImplementedError()
+
+
+    def __str__(self):
+        squares = []
+        for y, row in enumerate(self.board):
+            for x, piece in enumerate(row):
+                square = console(piece, BLACK if (y + x) % 2 else WHITE)
+                squares.append(square)
+            squares.append("\n")
+        return "".join(squares)
