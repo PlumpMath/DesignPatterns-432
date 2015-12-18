5e5eb112ef4376ac6cebcf48ed10736b3d95dda9
muupu <once645@gmail.com>
2015-12-19 00:59:07 +0800

    console

--- FactoryMethod/FactoryMethod4.py
+++ FactoryMethod/FactoryMethod4.py
@@ -17,3 +17,12 @@ def main():
         with open(filename, "w", encoding="utf-8") as file:
             file.write(sys.stdout.getvalue())
         print("wrote '{}'".format(filename), file=sys.__stdout__)
+
+if sys.platform.startswith("win"):
+    def console(char, background):
+        return char or " "
+    sys.stdout = io.StringIO()
+else:
+    def console(char, background):
+        return "\x1B[{}m{}\x1B[0m".format(
+                43 if background == BLACK else 47, char or " ")
