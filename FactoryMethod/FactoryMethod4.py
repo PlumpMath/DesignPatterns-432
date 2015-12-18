5d086e1285b9cfaf180cdf65a92dfbfab1b73de6
muupu <once645@gmail.com>
2015-12-18 23:19:28 +0800

    FactoryMethod4

---
+++ FactoryMethod/FactoryMethod4.py
@@ -0,0 +1,19 @@
+import io
+import itertools
+import os
+import sys
+import tempfile
+import unicodedata
+
+def main():
+    checkers = CheckersBoard()
+    print(checkers)
+
+    chess = ChessBoard()
+    print(chess)
+
+    if sys.platform.startswith("win"):
+        filename = os.path.join(tempfile.gettempdir(), "gameboard.txt")
+        with open(filename, "w", encoding="utf-8") as file:
+            file.write(sys.stdout.getvalue())
+        print("wrote '{}'".format(filename), file=sys.__stdout__)
