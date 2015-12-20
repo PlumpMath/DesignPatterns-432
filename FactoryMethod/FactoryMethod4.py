c90f84c77806a4b66b49165941c0ce44a014a1e5
muupu <once645@gmail.com>
2015-12-20 23:25:20 +0800

    AbstractBoard

--- FactoryMethod/FactoryMethod4.py
+++ FactoryMethod/FactoryMethod4.py
@@ -26,3 +26,23 @@ else:
     def console(char, background):
         return "\x1B[{}m{}\x1B[0m".format(
                 43 if background == BLACK else 47, char or " ")
+
+class AbstractBoard:
+
+    def __init__(self, rows, columns):
+        self.board = [[None for _ in range(columns)] for _ in range(rows)]
+        self.populate_board()
+
+
+    def populate_board(self):
+        raise NotImplementedError()
+
+
+    def __str__(self):
+        squares = []
+        for y, row in enumerate(self.board):
+            for x, piece in enumerate(row):
+                square = console(piece, BLACK if (y + x) % 2 else WHITE)
+                squares.append(square)
+            squares.append("\n")
+        return "".join(squares)
