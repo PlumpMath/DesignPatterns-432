8791d72201063a160fc4d3021bbc74875755d427
muupu <once645@gmail.com>
2015-12-21 22:55:42 +0800

    CheckersBoard

--- FactoryMethod/FactoryMethod4.py
+++ FactoryMethod/FactoryMethod4.py
@@ -46,3 +46,22 @@ class AbstractBoard:
                 squares.append(square)
             squares.append("\n")
         return "".join(squares)
+
+class CheckersBoard(AbstractBoard):
+
+    def __init__(self):
+        self.populate_board()
+
+
+    def populate_board(self): # Thanks to Doug Hellmann for the idea!
+        def black():
+            return create_piece(DRAUGHT, BLACK)
+        def white():
+            return create_piece(DRAUGHT, WHITE)
+        rows = ((None, black()), (black(), None), (None, black()),
+                (black(), None),            # 4 black rows
+                (None, None), (None, None), # 2 blank rows
+                (None, white()), (white(), None), (None, white()),
+                (white(), None))            # 4 white rows
+        self.board = [list(itertools.islice(
+            itertools.cycle(squares), 0, len(rows))) for squares in rows]
