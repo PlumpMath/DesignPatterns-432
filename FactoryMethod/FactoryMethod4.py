503bd5adea79fcfff2cbb5764fb20f2ca58fd585
muupu <once645@gmail.com>
2015-12-22 23:35:52 +0800

    ChessBoard

--- FactoryMethod/FactoryMethod4.py
+++ FactoryMethod/FactoryMethod4.py
@@ -65,3 +65,19 @@ class CheckersBoard(AbstractBoard):
                 (white(), None))            # 4 white rows
         self.board = [list(itertools.islice(
             itertools.cycle(squares), 0, len(rows))) for squares in rows]
+
+class ChessBoard(AbstractBoard):
+
+    def __init__(self):
+        super().__init__(8, 8)
+
+
+    def populate_board(self):
+        for row, color in ((0, BLACK), (7, WHITE)):
+            for columns, kind in (((0, 7), ROOK), ((1, 6), KNIGHT),
+                    ((2, 5), BISHOP), ((3,), QUEEN), ((4,), KING)):
+                for column in columns:
+                    self.board[row][column] = create_piece(kind, color)
+        for column in range(8):
+            for row, color in ((1, BLACK), (6, WHITE)):
+                self.board[row][column] = create_piece(PAWN, color)
